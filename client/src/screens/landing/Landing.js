e88f9f40be2a0413ebedfeb5064add52f0b387ed
porush <porush@porushs-MacBook-Air.local>
2023-03-30 03:12:38 +0530

    landing page start

---
+++ client/src/screens/landing/Landing.js
@@ -0,0 +1,26 @@
+import { Container } from '@chakra-ui/react'
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import Image from 'react-bootstrap/Image'
+import code_editor from '../../assets/images/code_editor.png'
+const Landing = () => {
+  return (
+    <div>
+      <Container style={{marginTop:"10%"}}>
+        <Row>
+          <Col className='mt-5'>
+            <Row><h1 className='text-white'>Explore</h1></Row>
+            <Row><h1 className='text-muted'>Contribute</h1></Row>
+            <Row><h1 className='text-danger'>Help grow the community</h1></Row>
+
+          </Col>
+          <Col>
+            <Image src={code_editor} fluid rounded/>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Landing
