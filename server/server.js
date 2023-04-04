2ce2fd2166841e652c7e3124865cb3996b46418f
ab-ankush <ankusha062@gmail.com>
2023-04-05 00:09:53 +0530

    updated server.js

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// import { router as userRouter } from "./routes/user.js";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// app.use("/api", userRouter);
+
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`serving on port ${PORT}`);
+});
