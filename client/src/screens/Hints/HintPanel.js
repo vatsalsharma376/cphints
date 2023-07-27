3b6423958dc66219a8ebb5908a992eb88ac945f3
ab-ankush <ankusha062@gmail.com>
2023-07-27 20:18:40 +0530

    Updated hints page

--- client/src/screens/Hints/HintPanel.js
+++ client/src/screens/Hints/HintPanel.js
@@ -1,19 +1,113 @@
-import React from "react";
-import { Row, Col, Card } from "react-bootstrap";
-import { HandThumbsUp, HandThumbsDown , CaretDown,CaretUp} from "react-bootstrap-icons";
+import React, { useState } from "react";
+import { Col, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { ListGroup, Stack } from "react-bootstrap";
-const tags = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8"];
-const HintPanel = ({ bgColor }) => {
+import "./HintsPannel.css";
+
+const tag1 = [
+  "dynamic programming",
+  "greedy algorithm",
+  "sliding window concept",
+  "greedy algorithm",
+  "sliding window concept",
+];
+
+const HintTags = ({ tags }) => {
   return (
-    <Col xs={4}>
+    <>
+      {tags.map((t) => {
+        return (
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 100, hide: 100 }}
+            overlay={<Tooltip id="button-tooltip">{t}</Tooltip>}
+          >
+            <div
+              className="p-1 m-1 rounded text-black"
+              style={{ fontSize: ".9em", backgroundColor: "#D9D9D9" }}
+            >
+              {t.length > 10 ? t.split(" ")[0] + "..." : t}
+            </div>
+          </OverlayTrigger>
+        );
+      })}
+    </>
+  );
+};
+
+const HintPanel = ({ bgColor }) => {
+  const [upvote, setUpvote] = useState(0);
+  const [downvote, setDownvote] = useState(0);
+
+  const setDownVoteButton = () => {
+    if (upvote) setUpvote((prev) => !prev);
+    setDownvote((prev) => !prev);
+  };
+
+  const setUpVoteButton = () => {
+    if (downvote) setDownvote((prev) => !prev);
+    setUpvote((prev) => !prev);
+  };
+
+  return (
+    <Col xs={5} md={4} lg={3}>
       <div
-        style={{ backgroundColor: bgColor}}
-        className="rounded m-4"
+        style={{ backgroundColor: bgColor, height: "10rem" }}
+        className="my-3 px-3 rounded"
       >
-        <CaretUp size={30}/>
-        30
-        <CaretDown size={30}/>
-        2
+        <Stack direction="horizontal" gap={3} className="h-100">
+          <div className="d-flex flex-column h-100 justify-content-evenly">
+            <div>
+              {/* <CaretUp size={30} /> */}
+              <h3
+                style={{ lineHeight: "0" }}
+                className="caretUpButton"
+                onClick={setUpVoteButton}
+              >
+                <i
+                  className={
+                    !upvote
+                      ? "bi bi-caret-up"
+                      : "bi bi-caret-up-fill text-success"
+                  }
+                ></i>
+              </h3>
+              <h6>10</h6>
+            </div>
+            <div>
+              <h6>10</h6>
+              <h3
+                style={{ lineHeight: "0" }}
+                className="caretDownButton"
+                onClick={setDownVoteButton}
+              >
+                <i
+                  className={
+                    !downvote
+                      ? "bi bi-caret-down"
+                      : "bi bi-caret-down-fill text-danger"
+                  }
+                ></i>
+              </h3>
+              {/* <CaretDown size={30} /> */}
+            </div>
+          </div>
+          <div style={{ width: "80%" }}>
+            <div className="d-flex  flex-wrap overflow-hidden">
+              <HintTags tags={tag1} />
+            </div>
+            <div
+              className="d-flex flex-column align-items-end text-muted"
+              // style={{ color: "#d9d9d9" }}
+            >
+              <p>
+                <i class="bi bi-person-fill"></i> ANKUSH
+              </p>
+              <p className="m-0">
+                <i class="bi bi-clock"></i> 6 days ago
+              </p>
+            </div>
+          </div>
+        </Stack>
       </div>
     </Col>
   );
--- client/src/screens/Hints/Hints.js
+++ client/src/screens/Hints/Hints.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from "react";
 import NavBar from "../../components/Navbar";
-import { Container, Row } from 'react-bootstrap';
-import HintPanel from './HintPanel';
+import { Container, Row } from "react-bootstrap";
+import HintPanel from "./HintPanel";
 const Hints = () => {
   const arr = Array(10).fill(0);
   return (
-    <div style={{backgroundColor:"#0F131A"}}>
-    <div>
-        <NavBar bg="black"/>
+    <div style={{ backgroundColor: "#0F131A" }} className="min-vh-100">
+      <NavBar bg="black" />
+      <div>
         <h1>Hints</h1>
-        <h3> Question Link</h3> 
-        <h3>Platform</h3>
-        <Container >
+        <h4>question link</h4>
+        <h4>platform</h4>
+        <Container>
           <Row>
-            {arr.map(()=><HintPanel bgColor="#1A1D24"/>)}
-            </Row>
-
+            {arr.map(() => (
+              <HintPanel bgColor="#1A1D24" />
+            ))}
+          </Row>
         </Container>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Hints
+export default Hints;
